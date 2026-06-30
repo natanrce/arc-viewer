d342708826960dbfbdb88361d4f362e96d79b6a3
Natan Rodrigues <natanrce@proton.me>
2026-06-30 15:39:05 -0300

    Initial Commit

---
+++ Arc Cache Viewer/Arc_Cache_ViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Arc_Cache_ViewerApp.swift
+//  Arc Cache Viewer
+//
+//  Created by Natan Rodrigues on 30/06/26.
+//
+
+import SwiftUI
+
+@main
+struct Arc_Cache_ViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Arc Cache Viewer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Arc Cache Viewer
+//
+//  Created by Natan Rodrigues on 30/06/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
